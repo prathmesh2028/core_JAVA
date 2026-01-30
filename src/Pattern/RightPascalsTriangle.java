8661e5332f9a6d629e890087b8859bae0222b8ff
PRATHMESH BADHE <badheprathmesh2028@gmail.com>
2026-01-30 13:00:17 +0530

    pattern

---
+++ src/Pattern/RightPascalsTriangle.java
@@ -0,0 +1,24 @@
+package Pattern;
+/*
+
+
+*
+  *
+*   *
+  *   *
+*   *
+  *
+*
+
+
+
+
+
+
+ */
+public class RightPascalsTriangle {
+    public static void main(String[] args) {
+        int n = 4;
+
+    }
+}
