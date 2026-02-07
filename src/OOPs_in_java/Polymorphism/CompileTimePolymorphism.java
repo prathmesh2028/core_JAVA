9cb6b5eef02e31e3cf044648946f57a2c37278ca
PRATHMESH BADHE <badheprathmesh2028@gmail.com>
2026-02-07 17:24:09 +0530

    compile time polymorphism i.e method overloading i.e static binding

---
+++ src/OOPs_in_java/Polymorphism/CompileTimePolymorphism.java
@@ -0,0 +1,19 @@
+package OOPs_in_java.Polymorphism;
+class Sum{
+    public int sum(int x , int y){
+        return (x+y);
+    }
+    public int sum(int x , int y , int z){
+        return (x+y+z);
+    }
+    public double sum(double x , double y){
+        return (x+y);
+    }
+}
+public class CompileTimePolymorphism {
+    public static void main(String[] args) {
+        Sum s = new Sum();
+        System.out.println(s.sum(10,20));
+        System.out.println(s.sum(10.5 , 20.5));
+    }
+}
