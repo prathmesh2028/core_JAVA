f22383df827cc0d83b00137b73111d2123f71edc
PRATHMESH BADHE <badheprathmesh2028@gmail.com>
2026-02-28 20:55:58 +0530

    all possible substrings

--- src/StringInJava/AllPossibleSubstrings.java
+++ src/StringInJava/AllPossibleSubstrings.java
@@ -2,11 +2,12 @@ package StringInJava;
 
 public class AllPossibleSubstrings {
     public static void main(String[] args) {
-        String s = "gopi";
+        String s = "prathmesh";
         for(int i = 0 ; i <= s.length() ; i++){
             for(int j = i+1 ; j <= s.length()  ; j++){
-                System.out.println(s.substring(i,j));
+                System.out.print(s.substring(i,j) + " ");
             }
+            System.out.println();
         }
     }
 }
