9c6a790edbf52ba66ad1404ac0680f3a9b61dc4b
PRATHMESH BADHE <badheprathmesh2028@gmail.com>
2026-02-28 20:27:35 +0530

    substring

---
+++ src/StringInJava/SubstringOfString.java
@@ -0,0 +1,10 @@
+package StringInJava;
+
+public class SubstringOfString {
+    public static void main(String[] args) {
+        String s = "Jaishankar";
+        System.out.println(s.substring(0));
+        System.out.println(s.substring(1 , 5)); // last one is not included always remeber
+
+    }
+}
