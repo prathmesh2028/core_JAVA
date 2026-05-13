673e290515d72ee79dc534a871903cb88de3fca6
PRATHMESH BADHE <badheprathmesh2028@gmail.com>
2026-05-13 07:13:20 +0530

    switch statement in java

--- src/Basics/SwitchStatement.java
+++ src/Basics/SwitchStatement.java
@@ -3,5 +3,32 @@ package Basics;
 public class SwitchStatement {
     public static void main(String[] args) {
 
+        int n = 1;
+
+        switch(n){
+            case 1:
+                System.out.println("monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("No day matched :)");
+
+        }
     }
 }
