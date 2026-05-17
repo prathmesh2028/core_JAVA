da644ffbfd5d8be552cf9688227b3d4440b18b74
PRATHMESH BADHE <badheprathmesh2028@gmail.com>
2026-05-17 18:05:45 +0530

    annotation in java

---
+++ src/OOPs_in_java/Annotations/Annotation.java
@@ -0,0 +1,25 @@
+package OOPs_in_java.Annotations;
+
+@Deprecated
+class A
+{
+    public void showTheDataWhichBelongsToThisClass()
+    {
+        System.out.println("in A show");
+    }
+}
+class B extends A
+{
+    @Override
+    public void showTheDataWhichBelongsToThisClass()
+    {
+        System.out.println("in A show");
+    }
+}
+
+public class Annotation {
+    public static void main(String[] args) {
+        B obj = new B();
+        obj.showTheDataWhichBelongsToThisClass();
+    }
+}
