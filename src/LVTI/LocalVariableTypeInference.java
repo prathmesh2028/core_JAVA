f95581bcb3e52c1f8f72a4067f30b8ffcbcff264
PRATHMESH BADHE <badheprathmesh2028@gmail.com>
2026-05-21 22:42:23 +0530

    Local variable Type inference

--- src/LVTI/LocalVariableTypeInference.java
+++ src/LVTI/LocalVariableTypeInference.java
@@ -2,6 +2,18 @@ package LVTI;
 
 public class LocalVariableTypeInference {
     public static void main(String[] args) {
+        int a = 9;
+        var b = 8; // java automatically understands that it is int
+
+
+
+        int c; // at least we know its type
+        var d = 10;
+
+
+        int nums[] = new int[10];
+        var num = new int[10];
+
 
     }
 }
